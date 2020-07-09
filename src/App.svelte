70c1e3d906087482d649047a5504daf2fca1f2d7
Ricardo Mattiazzi Baumgartner <ricardo.mattiazzi@rangle.io>
2020-07-09 15:12:11 -0400

    Add atep 5 initial state

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,24 @@
 <script>
+	import { crossfade } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	import { flip } from 'svelte/animate';
+
+	const [send, receive] = crossfade({
+		duration: d => Math.sqrt(d * 200),
+		fallback(node, params) {
+			const style = getComputedStyle(node);
+			const transform = style.transform === 'none' ? '' : style.transform;
+			return {
+				duration: 600,
+				easing: quintOut,
+				css: t => `
+					transform: ${transform} scale(${t});
+					opacity: ${t}
+				`
+			};
+		}
+	});
+
 	let uid = 1;
 
 	let todos = [
@@ -38,7 +58,11 @@
 	<div>
 		<h2>Todo</h2>
 		{#each todos.filter(t => !t.done) as todo (todo.id)}
-			<label>
+			<label
+				in:receive="{{key: todo.id}}"
+				out:send="{{key: todo.id}}"
+				animate:flip
+			>
 				<input type=checkbox on:change={() => mark(todo, true)}>
 				{todo.description}
 				<button on:click="{() => remove(todo)}">remove</button>
@@ -48,8 +72,13 @@
 	<div>
 		<h2>Done</h2>
 		{#each todos.filter(t => t.done) as todo (todo.id)}
-			<label class="done">
-				<input type=checkbox checked on:change={() => mark(todo, false)}>
+			<label
+				class="done"
+				in:receive="{{key: todo.id}}"
+				out:send="{{key: todo.id}}"
+				animate:flip
+			>
+			<input type=checkbox checked on:change={() => mark(todo, false)}>
 				{todo.description}
 				<button on:click="{() => remove(todo)}">remove</button>
 			</label>
@@ -105,8 +134,12 @@
 		background: no-repeat 50% 50% url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='%23676778' d='M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M17,7H14.5L13.5,6H10.5L9.5,7H7V9H17V7M9,18H15A1,1 0 0,0 16,17V10H8V17A1,1 0 0,0 9,18Z'%3E%3C/path%3E%3C/svg%3E");
 		background-size: 1.4em 1.4em;
 		border: none;
+		opacity: 0;
 		transition: opacity 0.2s;
 		text-indent: -9999px;
 		cursor: pointer;
+	}
+	label:hover button {
+		opacity: 1;
 	}
 </style>
